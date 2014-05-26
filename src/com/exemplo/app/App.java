7d78aac1276522d886281df30ff4485f7404f2d6
Jorge Feitosa <jorge.feitosa@dce.ufpb.br>
2014-05-26 09:18:16 -0300

    Adicionando linhas de testes

---
+++ src/com/exemplo/app/App.java
@@ -0,0 +1,43 @@
+package com.exemplo.app;
+
+public class App {
+
+	/**
+	 * @param args
+	 */
+	protected String nome;
+	protected int idade;
+	protected char sexo;
+	
+	public App(String n, int i, char s){
+		this.nome = n;
+		this.idade = i;
+		this.sexo = s;
+		
+	}
+	
+	public void setIdade(int idade){
+		this.idade = idade;
+	}
+	
+	public int getIdade(){
+		return idade;
+	}
+	
+	public int executaCalculoIdade(int idade1, int idade2){
+		
+		int total = idade1 + idade2;
+		return total;
+	}
+	
+	public int calculaFatorial(int n){
+		
+		int totalFatorial = 1;
+		
+		for( int i = n; i > 0;n--){
+			totalFatorial = n * totalFatorial;
+		}
+		
+		return totalFatorial;
+	}
+}
---
+++ src/com/exemplo/app/AppTest.java
@@ -0,0 +1,36 @@
+package com.exemplo.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest{
+	
+	App app = new App("Jorge",23,'M');
+
+	@Test
+	public void testObject() {
+		
+	
+		
+		int idade1 = 10;
+		int idade2 = 20;
+		int valorEsperado = 30;
+		
+		int valorFeito = app.executaCalculoIdade(idade1, idade2);
+		assertEquals(valorEsperado, valorFeito, 0);
+		
+	}
+	
+	public void testFat(){
+		
+		int fat = 3;
+		int resultadoEsperado = 6;
+		
+		int resultadoCalculado = app.calculaFatorial(fat);
+		assertEquals(resultadoEsperado, resultadoCalculado, 0);
+	}
+
+	
+
+}
