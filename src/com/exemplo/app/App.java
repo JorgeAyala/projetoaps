83cd25a5c13cb94fb31de045db38480b97fa9a97
Jorge Feitosa <jorge.feitosa@dce.ufpb.br>
2014-05-26 09:30:29 -0300

    Insercao de mais linhas de testes

--- src/com/exemplo/app/App.java
+++ src/com/exemplo/app/App.java
@@ -8,36 +8,36 @@ public class App {
 	protected String nome;
 	protected int idade;
 	protected char sexo;
-	
-	public App(String n, int i, char s){
+
+	public App(String n, int i, char s) {
 		this.nome = n;
 		this.idade = i;
 		this.sexo = s;
-		
+
 	}
-	
-	public void setIdade(int idade){
+
+	public void setIdade(int idade) {
 		this.idade = idade;
 	}
-	
-	public int getIdade(){
+
+	public int getIdade() {
 		return idade;
 	}
-	
-	public int executaCalculoIdade(int idade1, int idade2){
-		
+
+	public int executaCalculoIdade(int idade1, int idade2) {
+
 		int total = idade1 + idade2;
 		return total;
 	}
-	
-	public int calculaFatorial(int n){
-		
+
+	public int calculaFatorial(int n) {
+
 		int totalFatorial = 1;
-		
-		for( int i = n; i > 0;n--){
+
+		for (int i = n; n > 0; n--) {
 			totalFatorial = n * totalFatorial;
 		}
-		
+
 		return totalFatorial;
 	}
 }
--- src/com/exemplo/app/AppTest.java
+++ src/com/exemplo/app/AppTest.java
@@ -4,33 +4,30 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class AppTest{
-	
-	App app = new App("Jorge",23,'M');
+public class AppTest {
+
+	App app = new App("Jorge", 23, 'M');
 
 	@Test
 	public void testObject() {
-		
-	
-		
+
 		int idade1 = 10;
 		int idade2 = 20;
 		int valorEsperado = 30;
-		
+
 		int valorFeito = app.executaCalculoIdade(idade1, idade2);
 		assertEquals(valorEsperado, valorFeito, 0);
-		
+
 	}
 	
-	public void testFat(){
-		
-		int fat = 3;
+	@Test
+	public void testFat() {
+
+		int fat = 5;
 		int resultadoEsperado = 6;
-		
+
 		int resultadoCalculado = app.calculaFatorial(fat);
 		assertEquals(resultadoEsperado, resultadoCalculado, 0);
 	}
-
-	
 
 }
